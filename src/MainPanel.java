c4fb63222e252abeedf95e0d1487300ba7bb1e9d
madi ! <madiggudin@gmail.com>
2023-11-15 16:00:53 -0500

    added layout, changed fonts, (fonts still need work)

--- src/MainPanel.java
+++ src/MainPanel.java
@@ -14,18 +14,19 @@ public class MainPanel extends JPanel {
     public JTextArea bottomPanel;
 
     public MainPanel() {
+        Fonts.setUIFonts();
         this.setLayout(new BorderLayout());
 
         // Central panel
         this.centralPanel = new JPanel(new GridLayout(2,2));
         this.add(this.centralPanel, BorderLayout.CENTER);
 
+        this.centralPanel.add(new JTextArea("top left"));
         try{
             ToDoPanel toDoPanel = new ToDoPanel();
             this.centralPanel.add(toDoPanel);
         }
         catch(Exception e) {System.out.println("Error: " + e);}
-        this.centralPanel.add(new JTextArea("top right"));
         this.centralPanel.add(new JTextArea("bottom left"));
         this.centralPanel.add(new JTextArea("bottom right"));
 
@@ -34,5 +35,6 @@ public class MainPanel extends JPanel {
         this.add(this.bottomPanel, BorderLayout.PAGE_END);
         this.bottomPanel.setPreferredSize(new Dimension(this.getWidth(), 40));
         this.bottomPanel.setBackground(Color.GREEN);
+
     }
 }
