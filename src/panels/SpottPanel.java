a18001920ec4b7604f5013d0066fd379d578d989
serinh <serinch5@gmail.com>
2023-11-16 15:07:43 -0500

    made SpottPanel, deleted World, fixed Spott

--- src/panels/SpottPanel.java
+++ src/panels/SpottPanel.java
@@ -2,24 +2,45 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class SpottPanel extends JPanel {
+    
+    // instance variables
     Spott spott;
+    int length;
+    int height;
     int delay;
     World world;
 
-    public SpottPanel(int d)
+    // constructor
+    public SpottPanel(int x, int y, int d)
     {
-        spott = new Spott(new Pair(d, d), new Pair(d, d));
+        spott = new Spott(new Pair(d, d), new Pair(d, d), 20);
         
+        length = x;
+        height = y;
+
         // creating pauses for Spott
         delay = d; // TODO: randomize delay to have more organic movement
         ActionListener taskPerformer = new ActionListener()
         {
             public void actionPerformed(ActionEvent evt) {
-                spott.roaming(world, 1000000);
+                spott.roaming(1000000);
                 repaint();
             }
         };
         Timer timer = new Timer(delay, taskPerformer); // timer recurs every speed number of milliseconds
         timer.start();
+    }
+
+    // graphics
+    public void drawShapes()
+    {
+        // TODO: implement
+        // again, should be different from the one in Pong.java --> we're importing our own graphics
+    }
+
+    // movement
+    public void updateShapes(double time)
+    {
+        spott.roaming(time);
     }
 }
