2eebd133aeefeca930912b1c18121633f8ee5854
madi ! <madiggudin@gmail.com>
2023-11-16 12:12:54 -0500

    IT FINALLY WROSLKFKFSD

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
+import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Main {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
+            frame.setPreferredSize(new Dimension(1600, 900));
+            frame.pack();
             frame.setVisible(true);
-            frame.setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+            frame.setResizable(false);
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setTitle("✨ study corner 🌇");
 
