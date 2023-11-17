fc508d124beedc2ffe91f82fe6368514666849e2
amklinglerr <amklinglerr@gmail.com>
2023-11-16 23:36:10 -0500

    woo

--- src/panels/ToDoData.java
+++ src/panels/ToDoData.java
@@ -1,11 +1,9 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.io.PrintWriter;
 
 class ToDoData{
