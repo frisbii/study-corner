e76f9cf0302a457136a8a7494749e226597ec6ad
madi ! <madiggudin@gmail.com>
2023-11-26 01:28:13 -0500

    what an excellent wonderful world (timer has basic functionality)

--- src/panels/TimerPanel.java
+++ src/panels/TimerPanel.java
@@ -1,13 +1,17 @@
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
 import javax.swing.Timer;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class TimerPanel extends JPanel {
     
@@ -15,7 +19,8 @@ public class TimerPanel extends JPanel {
     private int timerSetting = 60;
     private int timerTimeLeft;
 
-    public JLabel timer;
+    private JLabel timer;
+    private JSpinner timeSetSpinner;
     
     public TimerPanel() {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
@@ -25,19 +30,25 @@ public class TimerPanel extends JPanel {
         this.swingTimer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                timerTimeLeft--;
-                updateTimerLabel();
+                if (timerTimeLeft == 0) {
+                    swingTimer.stop();
+                } else {
+                    timerTimeLeft--;
+                    updateTimerLabel();
+                }
             }
         });
         // -----
         
         // ----- Add components to base layout
+        this.add(Box.createVerticalGlue());
         // Timer title
         JLabel timerTitle = new JLabel("time left:");
-        timerTitle.setFont(Fonts.generateCutiveFont(10));
+        timerTitle.setFont(Fonts.generateCutiveFont(20));
         this.add(timerTitle);
         // Timer text
         this.timer = new JLabel();
+        this.timer.setFont(Fonts.generateCutiveFont(30));
         this.updateTimerLabel();
         this.add(this.timer);
         // Timer control buttons
@@ -67,7 +78,25 @@ public class TimerPanel extends JPanel {
         controlWrapper.add(resetButton);
         this.add(controlWrapper);
         // Timer set buttons
+        SpinnerModel model = new SpinnerNumberModel(60, 0, 6000, 1);
+        this.timeSetSpinner = new JSpinner(model);
+        this.timeSetSpinner.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {
+                JSpinner spinner = (JSpinner) (e.getSource());
+                timerSetting = (int) spinner.getValue();
+
+                swingTimer.stop();
+                timerTimeLeft = timerSetting;
+                updateTimerLabel();
+            }
+        });
+
+        JPanel setWrapper = new JPanel();
+        setWrapper.add(new JLabel("set time:"));
+        setWrapper.add(this.timeSetSpinner);
+        this.add(setWrapper);
         
+
         // -----
 
     }
