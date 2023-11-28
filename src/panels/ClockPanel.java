b64b02f84234a5eef7be1db4b106686fe3db0313
madi ! <madiggudin@gmail.com>
2023-11-28 16:30:18 -0500

    remove clockpanel paint

--- src/panels/ClockPanel.java
+++ src/panels/ClockPanel.java
@@ -1,3 +1,4 @@
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
@@ -5,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
@@ -13,26 +15,29 @@ public class ClockPanel extends JPanel {
     private int height = 400;
     private Timer timer;
 
+    private JLabel clockLabel;
+
     public ClockPanel() {
         timer = new Timer(1000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent ae) {
-                repaint();
+                updateClockLabel();
             }
         });
         timer.setCoalesce(true);
         timer.setRepeats(true);
         timer.start(); 
+        
+        this.setLayout(new BorderLayout());
+        this.clockLabel = new JLabel();
+        this.clockLabel.setFont(Fonts.generateCutiveFont(70));
+        this.clockLabel.setHorizontalAlignment(JLabel.CENTER);
+        this.updateClockLabel();
+        this.add(clockLabel, BorderLayout.CENTER);
     }
 
-    @Override
-    public void paintComponent(Graphics g) {
-        String time = new SimpleDateFormat("HH:mm.ss").format(new Date());
-
-        g.setColor(Color.WHITE);
-        g.fillRect(0, 0, width, height);
-        g.setColor(Color.BLACK);
-        g.setFont(Fonts.generateCutiveFont(65));
-        g.drawString(time, 60, 150);
+    private void updateClockLabel() {
+        this.clockLabel.setText(new SimpleDateFormat("HH:mm:ss").format(new Date()));
     }
+
 }
